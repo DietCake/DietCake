b0833ca9e529533e8a30d14da4b536e94e0a7552
xcezx <main.xcezx@gmail.com>
2013-06-12 18:53:57 +0900

    Add unit test for View class

---
+++ tests/Dietcake/ViewTest.php
@@ -0,0 +1,57 @@
+<?php
+use org\bovigo\vfs\vfsStream;
+
+class ViewTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $root;
+
+    public function setUp()
+    {
+        $this->root = vfsStream::setup('root');
+        define('VIEWS_DIR', vfsStream::url($this->root->getName()));
+
+        $this->controller = $this->getMockBuilder('Controller')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->view = new View($this->controller);
+    }
+
+    public function testRender()
+    {
+        $vFile = vfsStream::newFile('index.php');
+        $vFile->setContent(
+            <<<EOF
+Hello <?= $name ?> !!
+EOF
+        );
+        $this->root->addChild($vFile);
+
+        $this->view->vars = array('name' => 'John Doe');
+        $this->view->render('index');
+        $this->assertContains('Hello John Doe !!', $this->view->controller->output);
+    }
+
+    public function testExtract()
+    {
+        $vFile = vfsStream::newFile('index.php');
+        $vFile->setContent(
+            <<<EOF
+Hello <?= $name ?> !!
+EOF
+        );
+        $this->root->addChild($vFile);
+        $this->assertContains('Hello John Doe !!', View::extract(vfsStream::url($this->root->getName() . DIRECTORY_SEPARATOR . $vFile->getName()), array('name' => 'John Doe')));
+    }
+
+    /**
+     * @expectedException DCException
+     * @expectedExceptionMessage not_exists.php is not found
+     */
+    public function testDCExceptionOnExtract()
+    {
+        View::extract('not_exists.php', array());
+    }
+}
