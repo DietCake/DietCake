8231317f9aea8713f5bcb5039510b76db8376e97
xcezx <main.xcezx@gmail.com>
2013-11-06 02:31:08 +0900

    add Model::validate() tests

--- tests/Dietcake/ModelTest.php
+++ tests/Dietcake/ModelTest.php
@@ -10,8 +10,80 @@ class ModelTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('test', $model->bar);
     }
 
-    public function testValidate()
+    public function testValidateUseFunction()
     {
-        $this->markTestIncomplete();
+        $user = new User;
+        $user->validation = array(
+            'email' => array('invalid_email' => array('filter_var', FILTER_VALIDATE_EMAIL)),
+        );
+
+        $user->set(array('email' => 'john.doe@example.com'));
+        $this->assertTrue($user->validate());
+        $this->assertEquals($user->validation_errors, array(
+            'email' => array('invalid_email' => false),
+        ));
+
+        $user->set(array('email' => 'invalid.email'));
+        $this->assertFalse($user->validate());
+        $this->assertEquals($user->validation_errors, array(
+            'email' => array('invalid_email' => true),
+        ));
+    }
+
+    public function testValidateUseMethod()
+    {
+        $user = new User;
+        $user->validation = array(
+            'is_active' => array('not_active' => array('isActive')),
+        );
+
+        $user->set(array('is_active' => true));
+        $this->assertTrue($user->validate());
+        $this->assertEquals($user->validation_errors, array(
+            'is_active' => array('not_active' => false),
+        ));
+
+        $user->set(array('is_active' => false));
+        $this->assertFalse($user->validate());
+        $this->assertEquals($user->validation_errors, array(
+            'is_active' => array('not_active' => true),
+        ));
+    }
+
+    public function testValidateMultiple()
+    {
+        $user = new User;
+        $user->validation = array(
+            'email' => array(
+                'invalid_email' => array('filter_var', FILTER_VALIDATE_EMAIL),
+                'invalid_domain' => array('filter_var', FILTER_VALIDATE_REGEXP, array('options' => array('regexp' => '/@example\.com$/'))),
+            ),
+        );
+
+        $user->set(array('email' => 'john.doe@example.com'));
+        $this->assertTrue($user->validate());
+        $this->assertEquals($user->validation_errors, array(
+            'email' => array(
+                'invalid_email' => false,
+                'invalid_domain' => false,
+            ),
+        ));
+
+        $user->set(array('email' => 'jane.doe@example.jp'));
+        $this->assertFalse($user->validate());
+        $this->assertEquals($user->validation_errors, array(
+            'email' => array(
+                'invalid_email' => false,
+                'invalid_domain' => true,
+            ),
+        ));
+    }
+}
+
+class User extends Model
+{
+    public function isActive()
+    {
+        return (boolean) $this->is_active;
     }
 }
