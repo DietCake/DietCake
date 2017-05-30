6a96e7f5ee66a354a4e684994f1e87ff33b7dc4f
ha1t <ha1t@users.noreply.github.com>
2017-05-30 19:21:30 +0900

    PHP5.3対応

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,7 @@ require_once ROOT_DIR.'dietcake.php';
 if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
+
+if (!class_exists('\PHPUnit_Framework_TestCase') && class_exists('\PHPUnit\Framework\TestCase')) {
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+}
